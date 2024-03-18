39584848f9dbac2c30f02e0dcd23a28c2c3d71c3
frllelrtza <alfarrelelmaretza@gmail.com>
2024-03-19 02:48:30 +0700

    routes & views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Muhammad Al-Farrel Elmaretza',
+        'email' => 'alfarrelelmaretza@gmail.com',
+        'image' => 'wave to earth.jpg',
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
